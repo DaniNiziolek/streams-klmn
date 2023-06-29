90ca8bd92f6fa783d0e8c40a0fd00546685a1779
DaniNiziolek <101141774+DaniNiziolek@users.noreply.github.com>
2023-06-29 10:03:26 -0700

    working out an efficient way to return line plots of year-to-year change in values at a reach...
    This isn't strictly part of a qaqc report and I'm not working on it as actively as I am the missings/outliers/resamples/resampleCV scripts

--- Interannual_changes_in_measured_conditions.R
+++ Interannual_changes_in_measured_conditions.R
@@ -14,21 +14,35 @@ fig1 <- ggplot(DF, aes(x = Year, y = Total.Taxa.Richness,
   geom_line() +
   theme(legend.position = 'none') + # find a way to cram guides=collect into this pipe for when that inevitably becomes needed
   facet_wrap(Park ~ .,ncol=2,scales="free_x")+
-  scale_x_continuous(breaks = function(Year){
-    if(min(Year)=="2011"){seq(2011,2021,2)} #this is the bitch, this is the line that's pushing me
-    else{seq(2012,2021,1)}#RIGHT to the edge today. I hate her. She hates me. No one is having fun.
-  })
+  scale_x_continuous#(breaks = SEE BELOW)
  
 
 # If I make this a for loop saving to object P can I pdf(unlist(P),onefile=T) to get a different page for every set of plots?
 # If I do THAT can I set the page headers to loop through the list of variables? Or is there a cleaner way, might merit a Names list?
-
 
 pdf(' NAME .pdf')
 print(fig1)
 dev.off()
 
 
+# BELOW is here :) 
 
+# nonfunctional, all plots go to the else case (2011 tried in and out of quotes - same outcome)
+# also tried Year swapped for x, shouldn't matter at all but I did try
+function(x){
+  if(min(x)=="2011"){seq(2011,2021,2)} #this is the bitch, this is the line that's pushing me
+  else{seq(2012,2021,1)}#RIGHT to the edge today. I hate her. She hates me. No one is having fun.
+}
 
+# true case prints only 2011 (no other values on x), false case prints nothing
+function(x){
+ifelse(min(x) < 2012,
+       {seq(2011,2021,2)},
+       {seq(2012,2021,1)}) }
+
+# and if we combine the two,
+function(x){
+  ifelse(min(x)=="2011", {seq(2011,2021,2)} 
+         {seq(2012,2021,1)}) }
+# nah we get the same as the first ifelse. what the hell
 
